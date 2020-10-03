1799ab26c57d73b36126c4d79fd15263fd4066b1
jhorelrevilla <jhorel.revilla@ucsp.edu.pe>
2020-10-02 23:04:34 -0500

    last_update

---
+++ build/CMakeFiles/Octree.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Octree.dir/main.cpp.o"
+  "Octree"
+  "Octree.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Octree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/openglbinaries.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/openglbinaries"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/openglbinaries.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
